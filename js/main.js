268f629fc9c2dc2353349e1ebfa23bd604f85f3f
nobuhiro.mishima <nobuhiro.mishima@outlook.com>
2024-08-08 22:10:12 +0900

    ハンバーガーメニュー作成

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.navigation');
+const cover = document.querySelector('.cover');
 
-hamburger.addEventListener('click', function(){
-    navigation.classList.toggle('visible');
-});
+hamburger.addEventListener('click', () => navigation.classList.toggle('visible'));
+hamburger.addEventListener('click', () => cover.classList.toggle('cover-bg'));
+cover.addEventListener('click', () => navigation.classList.toggle('visible'));
+cover.addEventListener('click', () => cover.classList.toggle('cover-bg'));
