983f843a3f63334ab50064cdb017dae79334408a
nobuhiro.mishima <nobuhiro.mishima@outlook.com>
2024-08-11 00:55:29 +0900

    スクロールすると途中で表示される「トップへ戻るボタン」の実装

--- js/main.js
+++ js/main.js
@@ -6,3 +6,25 @@ hamburger.addEventListener('click', () => navigation.classList.toggle('visible')
 hamburger.addEventListener('click', () => cover.classList.toggle('cover-bg'));
 cover.addEventListener('click', () => navigation.classList.toggle('visible'));
 cover.addEventListener('click', () => cover.classList.toggle('cover-bg'));
+
+//スクロールの検知
+$(function() {
+    // 変数にクラスを入れる
+    var btn = $('.go_top');
+    
+    //スクロールしてページトップから1000に達したらボタンを表示
+    $(window).on('load scroll', function(){
+      if($(this).scrollTop() > 1000) {
+        btn.addClass('active');
+      }else{
+        btn.removeClass('active');
+      }
+    });
+  
+    //スクロールしてトップへ戻る
+    btn.on('click',function () {
+      $('body,html').animate({
+        scrollTop: 0
+      });
+    });
+  });
